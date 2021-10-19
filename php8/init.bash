d9f26bcdbb637d81cbb7553dc83d2dcaf0e23098
EugeneDronov <dronov-ep@yandex.ru>
2021-10-19 10:32:11 +0300

    Стартовый коммит. php8 и rootfs

---
+++ php8/init.bash
@@ -0,0 +1,20 @@
+# .bashrc
+
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+	. /etc/bashrc
+fi
+
+# User specific environment
+PATH="$HOME/.local/bin:$HOME/bin:$PATH"
+export PATH
+
+# Uncomment the following line if you don't like systemctl's auto-paging feature:
+# export SYSTEMD_PAGER=
+
+# User specific aliases and functions
+
+PS1='\[\e[0;32m\]\u@\h\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \[\e[1;32m\]\$\[\e[m\] \[\e[1;37m\]'
+
+php-fpm
+
